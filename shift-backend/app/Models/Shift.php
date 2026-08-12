d203024767f568935b3b4dfcbf7c240aed0a087d
JulianGarcia36 <jualgarfi5@gmail.com>
2026-08-12 10:54:06 -0500

    Fix: llave sin cerrar en Shift.php

--- shift-backend/app/Models/Shift.php
+++ shift-backend/app/Models/Shift.php
@@ -1,19 +1,19 @@
 <?php
-
+ 
 namespace App\Models;
-
+ 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+ 
 class Shift extends Model
 {
     use HasFactory;
-
-    protected $fillable = [
-    'employee_id', 
-    'date', 
-    'start_time', 
-    'end_time', 
-    'type', 
-    'color'
-];
+ 
+    protected $fillable = ['employee_id', 'date', 'start_time', 'end_time', 'type', 'color'];
+ 
+    // Relación: Un turno pertenece a un empleado
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+}
